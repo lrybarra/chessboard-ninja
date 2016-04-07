a37137e4e76a70ac58ff232a1cf6b8ed630da36f
Luis Ybarra <email": null>
2016-04-07 10:21:16 -0500

    add left move functionality

--- application.js
+++ application.js
@@ -3,14 +3,22 @@ left arrow  37  up arrow  38  right arrow 39  down arrow  40
 a 65  w 87  d 68  s 83
 */
 
-var move = function(direction){
+var moveLeft = function(fighter){
+  var $current_position = $('table').find('.'+ fighter);
+  if ($current_position.prev().prop('nodeName') == "TD"){
+    $current_position.removeClass(fighter);
+    $current_position.prev().addClass(fighter);
+  }
+}
 
+var move = function(fighter, direction){
+  moveLeft(fighter);
 }
 
 $(document).ready(function() {
   $(document).on('keyup', function(e){
     if (e.which === 37){
-      alert('yoooo');
+      move('fighter-1', 'left');
     } else if (e.which === 38) {
 
     } else if (e.which === 39) {
