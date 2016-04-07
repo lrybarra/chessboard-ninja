2d10c76cbbcf76e3d57dde3afc4915802aee8bc6
Luis Ybarra <email": null>
2016-04-07 10:08:07 -0500

    construct board and two basic characters

---
+++ application.js
@@ -0,0 +1,23 @@
+/*
+left arrow  37  up arrow  38  right arrow 39  down arrow  40
+a 65  w 87  d 68  s 83
+*/
+
+var move = function(direction){
+
+}
+
+$(document).ready(function() {
+  $(document).on('keyup', function(e){
+    if (e.which === 37){
+      alert('yoooo');
+    } else if (e.which === 38) {
+
+    } else if (e.which === 39) {
+
+    } else if (e.which === 40) {
+
+    }
+
+  });
+})
